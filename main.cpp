33287791b5e144859a70721879baa8af7890193a
SimonReese <59612841+SimonReese@users.noreply.github.com>
2021-01-11 22:50:00 +0100

    Il main non serve
    Ciaone

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "LineaFerroviaria.h"
-
-int main() {
-    LineaFerroviaria linea{ "C:\\Users\\Tommaso\\GitProjects\\GestionaleTranviario\\timetables.txt", "C:\\Users\\Tommaso\\GitProjects\\GestionaleTranviario\\line_description.txt" };
-    linea.esegui();
-}
